09334e4812c09c7e9708792531636a849b27cfb0
Laurens <lauverhaar@gmail.com>
2023-08-20 15:14:39 +0200

    feat: :sparkles: Add loadValue() and loadValues() functions to Memory

--- src/Memory.ts
+++ src/Memory.ts
@@ -4,4 +4,14 @@ export class Memory {
 	public data = new Uint8Array(MEMORY_SIZE);
 
 	constructor() {}
+
+	loadValue(address: number, value?: number) {
+		this.data[address] = value ?? 0;
+	}
+
+	loadValues(values: Record<number, number | undefined>) {
+		Object.entries(values).forEach(([address, value]) => {
+			this.data[parseInt(address)] = value ?? 0;
+		});
+	}
 }
